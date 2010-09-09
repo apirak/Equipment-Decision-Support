d2ed92aa87ed2d4d65dc0223b2b2d4868e7dcead
Apirak Panatkool <apirakb@gmail.com>
2010-09-09 22:35:58 +0700

    new general form and load xml

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,24 @@
+class SiteController < ApplicationController
+  def show
+    @site = Site.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.xml  #{ render :xml => @site }
+    end
+  end
+end
+
+
+# <message success="true"> 
+# <contact> 
+#     <name> 
+#         <first>Jack</first> 
+#         <last>Slocum</last> 
+#     </name> 
+#     <company>Ext JS</company> 
+#     <email>support@extjs.com</email> 
+#     <state>OH</state> 
+#     <dob>04/15/2007</dob> 
+# </contact> 
+# </message>
--- public/javascripts/information_interface.js
+++ public/javascripts/information_interface.js
@@ -8,6 +8,62 @@ var myData = [
      * Custom function used for column renderer
      * @param {Object} val
      */
+
+		Ext.namespace('Ext.exampledata');
+
+		Ext.exampledata.states = [
+		        ['AL', 'Alabama', 'The Heart of Dixie'],
+		        ['AK', 'Alaska', 'The Land of the Midnight Sun'],
+		        ['AZ', 'Arizona', 'The Grand Canyon State'],
+		        ['AR', 'Arkansas', 'The Natural State'],
+		        ['CA', 'California', 'The Golden State'],
+		        ['CO', 'Colorado', 'The Mountain State'],
+		        ['CT', 'Connecticut', 'The Constitution State'],
+		        ['DE', 'Delaware', 'The First State'],
+		        ['DC', 'District of Columbia', "The Nation's Capital"],
+		        ['FL', 'Florida', 'The Sunshine State'],
+		        ['GA', 'Georgia', 'The Peach State'],
+		        ['HI', 'Hawaii', 'The Aloha State'],
+		        ['ID', 'Idaho', 'Famous Potatoes'],
+		        ['IL', 'Illinois', 'The Prairie State'],
+		        ['IN', 'Indiana', 'The Hospitality State'],
+		        ['IA', 'Iowa', 'The Corn State'],
+		        ['KS', 'Kansas', 'The Sunflower State'],
+		        ['KY', 'Kentucky', 'The Bluegrass State'],
+		        ['LA', 'Louisiana', 'The Bayou State'],
+		        ['ME', 'Maine', 'The Pine Tree State'],
+		        ['MD', 'Maryland', 'Chesapeake State'],
+		        ['MA', 'Massachusetts', 'The Spirit of America'],
+		        ['MI', 'Michigan', 'Great Lakes State'],
+		        ['MN', 'Minnesota', 'North Star State'],
+		        ['MS', 'Mississippi', 'Magnolia State'],
+		        ['MO', 'Missouri', 'Show Me State'],
+		        ['MT', 'Montana', 'Big Sky Country'],
+		        ['NE', 'Nebraska', 'Beef State'],
+		        ['NV', 'Nevada', 'Silver State'],
+		        ['NH', 'New Hampshire', 'Granite State'],
+		        ['NJ', 'New Jersey', 'Garden State'],
+		        ['NM', 'New Mexico', 'Land of Enchantment'],
+		        ['NY', 'New York', 'Empire State'],
+		        ['NC', 'North Carolina', 'First in Freedom'],
+		        ['ND', 'North Dakota', 'Peace Garden State'],
+		        ['OH', 'Ohio', 'The Heart of it All'],
+		        ['OK', 'Oklahoma', 'Oklahoma is OK'],
+		        ['OR', 'Oregon', 'Pacific Wonderland'],
+		        ['PA', 'Pennsylvania', 'Keystone State'],
+		        ['RI', 'Rhode Island', 'Ocean State'],
+		        ['SC', 'South Carolina', 'Nothing Could be Finer'],
+		        ['SD', 'South Dakota', 'Great Faces, Great Places'],
+		        ['TN', 'Tennessee', 'Volunteer State'],
+		        ['TX', 'Texas', 'Lone Star State'],
+		        ['UT', 'Utah', 'Salt Lake State'],
+		        ['VT', 'Vermont', 'Green Mountain State'],
+		        ['VA', 'Virginia', 'Mother of States'],
+		        ['WA', 'Washington', 'Green Tree State'],
+		        ['WV', 'West Virginia', 'Mountain State'],
+		        ['WI', 'Wisconsin', "America's Dairyland"],
+		        ['WY', 'Wyoming', 'Like No Place on Earth']
+		    ];
 function change(val){
     if(val > 0){
         return '<span style="color:green;">' + val + '</span>';
@@ -21,6 +77,20 @@ function change(val){
      * Custom function used for column renderer
      * @param {Object} val
      */
+
+// A reusable error reader class for XML forms
+Ext.form.XmlErrorReader = function(){
+    Ext.form.XmlErrorReader.superclass.constructor.call(this, {
+            record : 'field',
+            success: '@success'
+        }, [
+            'id', 'msg'
+        ]
+    );
+};
+Ext.extend(Ext.form.XmlErrorReader, Ext.data.XmlReader);
+		
+		
 function pctChange(val){
     if(val > 0){
         return '<span style="color:green;">' + val + '%</span>';
@@ -32,19 +102,13 @@ function pctChange(val){
 
 // create the data store
 var store = new Ext.data.ArrayStore({
-    fields: [
-    {
+    fields: [{
         name: 'equipment'
-    },
-
-    {
+    },{
         name: 'price'
-    },
-
-    {
+    },{
         name: 'suggest'
-    }
-    ]
+    }]
 });
 
 // manually load local data
@@ -95,6 +159,114 @@ var grid = new Ext.grid.GridPanel({
     }
 });
 
+/********************/
+/* FORM             */
+/********************/
+
+Ext.QuickTips.init();
+
+ // turn on validation errors beside the field globally
+ Ext.form.Field.prototype.msgTarget = 'side';
+
+ var fs = new Ext.FormPanel({
+     frame: true,
+     title:'General',
+     labelAlign: 'right',
+     labelWidth: 85,
+     width:340,
+     waitMsgTarget: true,
+
+     // configure how to read the XML Data
+     reader : new Ext.data.XmlReader({
+         record : 'contact',
+         success: '@success'
+     }, [
+         {name: 'first', mapping:'name/first'}, // custom mapping
+         {name: 'last', mapping:'name/last'},
+         'company', 'email', 'state',
+         {name: 'dob', type:'date', dateFormat:'m/d/Y'} // custom data types
+     ]),
+
+     // reusable eror reader class defined at the end of this file
+     errorReader: new Ext.form.XmlErrorReader(),
+
+     items: [
+         new Ext.form.FieldSet({
+             title: 'Contact Information',
+             autoHeight: true,
+             defaultType: 'textfield',
+             items: [{
+                     fieldLabel: 'First Name',
+                     emptyText: 'First Name',
+                     name: 'first',
+                     width:190
+                 }, {
+                     fieldLabel: 'Last Name',
+                     emptyText: 'Last Name',
+                     name: 'last',
+                     width:190
+                 }, {
+                     fieldLabel: 'Company',
+                     name: 'company',
+                     width:190
+                 }, {
+                     fieldLabel: 'Email',
+                     name: 'email',
+                     vtype:'email',
+                     width:190
+                 },
+
+                 new Ext.form.ComboBox({
+                     fieldLabel: 'State',
+                     hiddenName:'state',
+                     store: new Ext.data.ArrayStore({
+                         fields: ['abbr', 'state'],
+                         data : Ext.exampledata.states // from states.js
+                     }),
+                     valueField:'abbr',
+                     displayField:'state',
+                     typeAhead: true,
+                     mode: 'local',
+                     triggerAction: 'all',
+                     emptyText:'Select a state...',
+                     selectOnFocus:true,
+                     width:190
+                 }),
+
+                 new Ext.form.DateField({
+                     fieldLabel: 'Date of Birth',
+                     name: 'dob',
+                     width:190,
+                     allowBlank:false
+                 })
+             ]
+         })
+     ]
+ });
+
+ // simple button add
+ fs.addButton('Load', function(){
+     fs.getForm().load({url:'/site/6.xml', waitMsg:'Loading', method:'get'});
+ });
+
+ // explicit add
+ var submit = fs.addButton({
+     text: 'Submit',
+     disabled:true,
+     handler: function(){
+         fs.getForm().submit({url:'xml-errors.xml', waitMsg:'Saving Data...', submitEmptyText: false});
+     }
+ });
+
+fs.on({
+    actioncomplete: function(form, action){
+        if(action.type == 'load'){
+            submit.enable();
+        }
+    }
+});
+
+// form
 
 var tab_site = new Ext.TabPanel({
     activeTab: 0,
@@ -109,10 +281,7 @@ var tab_site = new Ext.TabPanel({
     defaults:{
         autoScroll: true
     },
-    items:[{
-        title: 'General',
-        html: "My content was added during construction."
-    },grid,{
+    items:[fs,grid,{
         title: 'Suggestion',
         xtype:'htmleditor'
     //anchor:'98%',
@@ -138,12 +307,7 @@ var information = {
         align : 'stretch',
         pack  : 'start'
     },
-    items:[tab_site],
-    bbar: {
-        items:[{
-            text: 'Save',
-            iconCls: 'save'
-        }]
-    }
+    items:[tab_site]
 };
+
 
